e7c28d18887d16a905a58bdd5499623e756346cd
Andreas Haupt <indraos@freenet.de>
2018-10-20 13:40:56 +0200

    Added several columns instead of dict for data

--- DataLoader.py
+++ DataLoader.py
@@ -12,7 +12,7 @@ class DataLoader:
 	"""
 	Class loading and joining data from Reuters and Stocks Data Set
 	"""
-	def __init__(self, subcompany_list=['AAPL'], diff=30, tol=.1, subset=['reuters_us_news_20171010.csv']):
+	def __init__(self, subcompany_list=['AAPL'], diff=30, tol=.5, subset=['reuters_us_news_20171010.csv']):
 		self.subcompany_list = subcompany_list
 		self.diff = diff
 		self.tol = tol
@@ -31,32 +31,39 @@ class DataLoader:
 			hours = a[3].split(':')
 			month_dict = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12}
 			return datetime.datetime(int(a[5]), int(month_dict[a[1]]), int(a[2]), int(hours[0]), int(hours[1]))
-		except IndexError as error:
+		except IndexError as err:
 			print(a)
 			return pd.NaT
+		except ValueError as err:
+			print("Malformed date ", err)
 			
 	def __transform_close_prices(self, data, diff):
 		data.set_index(pd.DatetimeIndex(data['date']),inplace=True)
 		data['close'] = pd.to_numeric(data['close'])
-		data['target'] = np.sign(data['close'].pct_change(diff))
+		cutoff = lambda x: 1 if np.sign(x) > self.tol else 0 if np.sign(x) < -self.tol else .5
+		data['target'] = data['close'].pct_change(diff).map(cutoff)
 		data.drop(['close','date'], axis=1, inplace=True)
 		return data
 
 	def __load_labels(self):
-		data_dict = {fn.split('_')[0]: pd.DataFrame(columns=['date','time','close']) for fn in os.listdir(self.path_labels)}
+		data_dict = {}
 		for filename in os.listdir(self.path_labels):
 			ticker = filename.split('_')[0]
 			if ticker in self.subcompany_list:
-				try:
-					data_dict[ticker] = data_dict[ticker].append(self.__load_label(filename))
-				except pd.io.common.EmptyDataError:
-					print(filename, " is empty and has been skipped.")
-		for i in data_dict:
-			if 'time' in data_dict[i].columns:
-				data_dict[i].drop('time', axis=1, inplace=True)
-			self.__transform_close_prices(data_dict[i], self.diff)
-		self.labels_dict = data_dict
-		return data_dict
+				if ticker in data_dict:
+					try:
+						data_dict[ticker] = data_dict[ticker].append(self.__load_label(filename))
+					except pd.io.common.EmptyDataError:
+						print(filename, " is empty and has been skipped.")
+				else:
+					data_dict[ticker] = self.__load_label(filename)
+		for ticker in data_dict:
+			if 'time' in data_dict[ticker].columns:
+				data_dict[ticker].drop('time', axis=1, inplace=True)
+			self.__transform_close_prices(data_dict[ticker], self.diff)
+			data_dict[ticker].columns = [ticker]
+		self.labels = data_dict.values()
+		return self.labels
 
 	def __load_label(self, filename):
 		data = pd.read_csv(os.path.join(self.path_labels, filename), usecols=[2,3,7], dtype=str)
@@ -77,26 +84,26 @@ class DataLoader:
 	def load(self, path_data, path_labels):
 		self.path_data = path_data
 		self.path_labels = path_labels
+		labels = self.__load_labels()
+		labels = pd.concat(labels)
 		data = self.__load_data().sort_index()
-		self.data = {k: pd.merge(data,v, how='inner', left_index=True, right_index=True) for k, v in self.__load_labels().items()}
-		return {k: pd.merge(data,v, how='inner', left_index=True, right_index=True) for k, v in self.__load_labels().items()}
+		self.data = pd.merge(data, labels, how='inner', left_index=True, right_index=True)
+		return self.data
 
 	def save(self, path):
-		for key, value in self.data.items():
-			for year in range(2009, 2018):
-				for month in range(1,13):
-					filename = os.path.join(str(key),str(year),str(month))
-					start_date = str(year) + '-' + str(month) + '-01'
-					end_date = str(year) + '-' + str(month) + '-28'
-					os.makedirs(os.path.dirname(os.path.join(path,filename)), exist_ok=True)
-					with open(os.path.join(path,filename), "wb") as out_file:
-					    pickle.dump(self.data[key].loc[start_date:end_date], out_file)
+		for year in range(2009, 2018):
+			for month in range(1,13):
+				filename = os.path.join(str(year),str(month))
+				start_date = str(year) + '-' + str(month) + '-01'
+				end_date = str(year) + '-' + str(month) + '-28'
+				os.makedirs(os.path.dirname(os.path.join(path,filename)), exist_ok=True)
+				with open(os.path.join(path,filename), "wb") as out_file:
+				    pickle.dump(self.data.loc[start_date:end_date], out_file)
 
 	def calculate_embedding(self):
-		for key, data in self.data.items():
-			data['Word Embedding'] = data['abstract'].map(nltk.word_tokenize)
+		self.data['Word Embedding'] = self.data['abstract'].map(nltk.word_tokenize)
 
-data = DataLoader()
-a = data.load('../Reuters_US', '../StocksMinute')
+data = DataLoader(subcompany_list=['AAPL', 'YHOO'], subset=['reuters_us_news_20171010.csv','reuters_us_news_20171011.csv'])
+a = data.load('/Volumes/Elements/Nachrichten und Kurse/Reuters_US', '/Volumes/Elements/Nachrichten und Kurse/StocksMinute')
 data.calculate_embedding()
 data.save(r'./data')
