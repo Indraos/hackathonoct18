42ec66135018e33aef6cb17edd3304404776a45f
fkarl <preFrontalC0rtex!nov>
2018-10-20 09:21:02 +0200

    sample file

--- load_data.py
+++ load_data.py
@@ -3,11 +3,13 @@ import pickle
 import numpy as np
 import datetime
 import os
+from os import listdir
+from os.path import isfile, join
 import dateutil.parser as pa
 
 
-path_labels = '/Volumes/Elements/Nachrichten und Kurse/StocksMinute'
-path_data = '/Volumes/Elements/Nachrichten und Kurse/Reuters_US'
+path_labels = '../StocksMinute'
+path_data = '../Reuters_US'
 
 def load_labels(path, subcompany_list=['AAPL']):
 	data_dict = {fn.split('_')[0]: pd.DataFrame(columns=['date','time','close']) for fn in os.listdir(path)}
@@ -42,24 +44,35 @@ def transform_data(data, diff):
 		return data
 
 def transform_time_text(timestring):
-	if timestring != 'nan':
+	if timestring != 'nan' and timestring != "" and timestring != 'NaT':
 		a = timestring.split(' ')
 		hours = a[3].split(':')
-		month_dict = {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5,'Jun':6,'Jul':7,'Aug':8,'Sep':9,'Oct':10,'Nov':11,'Dec':12}
-		return datetime.datetime(int(a[5]),int(month_dict[a[1]]),int(a[2]),int(hours[0]),int(hours[1]))
+		month_dict = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12}
+		return datetime.datetime(int(a[5]), int(month_dict[a[1]]), int(a[2]), int(hours[0]), int(hours[1]))
+
 
 def load_sentences(path, subset=['reuters_us_news_20171010.csv']):
 	data = pd.DataFrame(columns=['date','abstract','text'])
 	for filename in os.listdir(path):
 		if filename in subset:
+			print(filename)
 			data = data.append(pd.read_csv(path + '/' + filename, sep=';', usecols=[0,1,2], names=['date','abstract','text']))
 			data['date'] = data['date'].map(str).map(transform_time_text)
 			data.set_index(pd.DatetimeIndex(data['date']),inplace=True)
 	return data
 
-def join_data(path_labels, path_data):
+def join_data(path_labels, path_data, subset):
 	labels = transform_data(load_labels(path_labels), 30)['AAPL'].sort_index()
-	data = load_sentences(path_data).sort_index()
+	data = load_sentences(path_data, subset).sort_index()
 	return labels, data, pd.merge(data,labels, how='inner', left_index=True, right_index=True)
 
 
+
+YEAR = '2017'
+for i in range(1, 13, 1):
+	this_year_and_month = YEAR + str(i).zfill(2)
+	all_paths = [f for f in listdir(path_data) if isfile(join(path_data, f))]
+	paths_from_one_month = [path for path in all_paths if this_year_and_month in path]
+	print(paths_from_one_month)
+	_,_,merde_data = join_data(path_labels,path_data, paths_from_one_month)
+	print(merde_data)
