28a8061dd2ea632af24637268f9a5719b370325b
Andreas Haupt <indraos@freenet.de>
2018-10-20 12:34:30 +0200

    Export

--- DataLoader.py
+++ DataLoader.py
@@ -6,6 +6,7 @@ import os
 from os import listdir
 from os.path import isfile, join
 import dateutil.parser as pa
+import nltk
 
 class DataLoader:
 	"""
@@ -17,9 +18,23 @@ class DataLoader:
 		self.tol = tol
 		self.subset = subset
 
-	def __transform_time_reuters(self, timestring):
-		return datetime.datetime(int(timestring[:4]), int(timestring[4:6]), int(timestring[6:8]), int(timestring[8:10]), int(timestring[10:12]))
+	def __transform_time_stocks(self, timestring):
+		try:
+			return datetime.datetime(int(timestring[:4]), int(timestring[4:6]), int(timestring[6:8]), int(timestring[8:10]), int(timestring[10:12]))
+		except ValueError as err:
+			print(err)
+			return pd.NaT
 
+	def __transform_time_reuters(self, timestring):
+		a = timestring.split(' ')
+		try:
+			hours = a[3].split(':')
+			month_dict = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12}
+			return datetime.datetime(int(a[5]), int(month_dict[a[1]]), int(a[2]), int(hours[0]), int(hours[1]))
+		except IndexError as error:
+			print(a)
+			return pd.NaT
+			
 	def __transform_close_prices(self, data, diff):
 		data.set_index(pd.DatetimeIndex(data['date']),inplace=True)
 		data['close'] = pd.to_numeric(data['close'])
@@ -33,7 +48,7 @@ class DataLoader:
 			ticker = filename.split('_')[0]
 			if ticker in self.subcompany_list:
 				try:
-					data_dict[ticker] = data_dict[ticker].append(self.__load_file(filename))
+					data_dict[ticker] = data_dict[ticker].append(self.__load_label(filename))
 				except pd.io.common.EmptyDataError:
 					print(filename, " is empty and has been skipped.")
 		for i in data_dict:
@@ -44,9 +59,9 @@ class DataLoader:
 		return data_dict
 
 	def __load_label(self, filename):
-		data = pd.read_csv(self.path_labels + '/' + filename, usecols=[2,3,7], dtype=str)
+		data = pd.read_csv(os.path.join(self.path_labels, filename), usecols=[2,3,7], dtype=str)
 		data.columns = ['date','time','close']
-		data['date'] = (data['date'].map(str) + data['time'].map(str)).map(transform_time)
+		data['date'] = (data['date'].map(str) + data['time'].map(str)).map(self.__transform_time_stocks)
 		data = data.dropna(axis=0)
 		return data
 
@@ -54,7 +69,7 @@ class DataLoader:
 		data = pd.DataFrame(columns=['date','abstract','text'])
 		for filename in os.listdir(self.path_data):
 			if filename in self.subset:
-				data = data.append(pd.read_csv(self.path_data + '/' + filename, sep=';', usecols=[0,1,2], names=['date','abstract','text']))
+				data = data.append(pd.read_csv(os.path.join(self.path_data,filename), sep=';', usecols=[0,1,2], names=['date','abstract','text']))
 		data['date'] = data['date'].map(str).map(self.__transform_time_reuters)
 		data.set_index(pd.DatetimeIndex(data['date']), inplace=True)
 		return data
@@ -66,7 +81,22 @@ class DataLoader:
 		self.data = {k: pd.merge(data,v, how='inner', left_index=True, right_index=True) for k, v in self.__load_labels().items()}
 		return {k: pd.merge(data,v, how='inner', left_index=True, right_index=True) for k, v in self.__load_labels().items()}
 
+	def save(self):
+		for key, value in self.data.items():
+			for year in range(2009, 2018):
+				for month in range(1,13):
+					filename = os.path.join(str(key),str(year),str(month))
+					start_date = str(year) + '-' + str(month) + '-01'
+					end_date = str(year) + '-' + str(month) + '-28'
+					os.makedirs(os.path.dirname(filename), exist_ok=True)
+					with open(filename, "wb") as out_file:
+					    pickle.dump(self.data[key].loc[start_date:end_date], out_file)
 
+	def calculate_embedding(self):
+		for key, data in self.data.items():
+			data['Word Embedding'] = data['abstract'].map(nltk.word_tokenize)
 
 data = DataLoader()
 a = data.load('/Volumes/Elements/Nachrichten und Kurse/Reuters_US', '/Volumes/Elements/Nachrichten und Kurse/StocksMinute')
+data.calculate_embedding()
+data.save()
